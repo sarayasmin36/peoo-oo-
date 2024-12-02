a9fb61c33a83df2d27b737d21eda31efe736edec
Sara <sara.y@escolar.ifrn.edu.br>
2024-12-02 08:11:38 -0300

    xcvzvdasd

--- prova 3bi/q1/Main.ts
+++ prova 3bi/q1/Main.ts
@@ -3,13 +3,17 @@ import { Gerente } from "./Gerente";
 import { Empresa } from "./Empresa";
 
 
-const funcionario1 = new Funcionario('Ana Rosa', 3000, 'ajudante');
-const funcionario2 = new Funcionario('Ana', 3030, 'ajudante da ajudante');
-const funcionario3 = new Funcionario('Rosa', 3500, 'ajudante que ajuda');
-const gerente1 = new Gerente('Julia', 5000, 'Gerente', 10);
-const gerente2 = new Gerente('Santana', 5500, 'Gerente', 10);
+const empresa = new Empresa("TechCorp");
 
-let vetor2 : Gerente[] = [gerente1, gerente2];
-let empresa = new Empresa(vetor2);
+// Criando funcionários
+const funcionario1 = new Funcionario("Ana", 3000, "Desenvolvedora");
+const funcionario2 = new Funcionario("Carlos", 4500, "Analista");
+const funcionario3 = new Funcionario("Mariana", 5000, "Gerente");
 
+// Adicionando funcionários à empresa
+empresa.adicionarFuncionario(funcionario1);
+empresa.adicionarFuncionario(funcionario2);
+empresa.adicionarFuncionario(funcionario3);
+
+// Listando funcionários
 empresa.listarFuncionarios();
